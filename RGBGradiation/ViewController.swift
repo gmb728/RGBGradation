6070f4109fcf72784e1f7347ec268044c854429a
Chang Sophia <you@example.com>
2018-11-10 16:53:13 +0800

    Initial Commit

---
+++ RGBGradiation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RGBGradiation
+//
+//  Created by Chang Sophia on 11/10/18.
+//  Copyright © 2018 Chang Sophia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
